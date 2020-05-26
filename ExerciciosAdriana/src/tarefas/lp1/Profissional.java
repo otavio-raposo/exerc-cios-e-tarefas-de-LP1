85b1153e02a1a8b7c6720d91196fe06a9abe61aa
otavio-raposo <yuusak.otavio@gmail.com>
2020-05-26 20:19:39 -0300

    Tentativa nº 2 de envio
    Onde tento novamente enviar o conteúdo criado em Java

--- ExerciciosAdriana/src/tarefas/lp1/Profissional.java
+++ ExerciciosAdriana/src/tarefas/lp1/Profissional.java
@@ -1,5 +1,47 @@
 package tarefas.lp1;
 
-public class SeisProfessor {
+public class Profissional {
+	
+	private String profissao;
+	private int salario;
+	private String especializacao;
+	
+	public void trabalhou() {
+		System.out.println("O profissional trabalhou hoje");
+	}
+	
+	public void pagamento() {
+		System.out.println("O profissional foi pago hoje");
+	}
+	
+	public void demissao() {
+		System.out.println("O profissional foi demitido hoje");
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public int getSalario() {
+		return salario;
+	}
+
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+
+	public String getEspecializacao() {
+		return especializacao;
+	}
+
+	public void setEspecializacao(String especializacao) {
+		this.especializacao = especializacao;
+	}
+	
+	
 
 }
